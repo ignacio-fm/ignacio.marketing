eff577635da0c3bcfda84337afb28ba659818c2b
Nacho <nachofernandezmillera@gmail.com>
2023-05-26 19:57:28 +0200

    Add robots.txt file and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ignacio.info',
+  site: 'https://ignacio.marketing',
   integrations: [tailwind(), sitemap(), mdx()]
 });
