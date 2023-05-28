041490402b3134e8211e38e66b17a3ce32c0df24
Nacho <nachofernandezmillera@gmail.com>
2023-05-28 03:00:25 +0200

    Some changes

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="astro/client" />
+
+// types.ts
+export type Frontmatter = {
+    postTitle: string;
+    postDescription: string;
+    publishedTime: string;
+    tags: string[];
+};
